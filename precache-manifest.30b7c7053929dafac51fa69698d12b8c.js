005bfab4138c37ae7594e4ee5fbe5872299bab6b
fito <fifodominguez@gmail.com>
2019-05-22 21:10:36 -0600

    Updates

---
+++ precache-manifest.30b7c7053929dafac51fa69698d12b8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bf81fe0d475cc1a0e5653fda0ce9e2",
+    "url": "/citasreact/index.html"
+  },
+  {
+    "revision": "344bc52b64c241e2c9a8",
+    "url": "/citasreact/static/css/main.4e9b3b55.chunk.css"
+  },
+  {
+    "revision": "a6ea6394b56e4f7d16dc",
+    "url": "/citasreact/static/js/2.cb5cb5a1.chunk.js"
+  },
+  {
+    "revision": "344bc52b64c241e2c9a8",
+    "url": "/citasreact/static/js/main.7e63c81a.chunk.js"
+  },
+  {
+    "revision": "537e03e3798c043a7ebc",
+    "url": "/citasreact/static/js/runtime~main.4f46ca0c.js"
+  }
+]);
